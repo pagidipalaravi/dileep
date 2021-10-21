22cab22fd830b72bcbdf49a8a154e3b0a2688917
udaykiranmallela3 <chowdarydileep2@gmail.com>
2021-10-21 22:17:36 +0530

    create employee
    create employee form

--- apps/hr/createemployee.js
+++ apps/hr/createemployee.js
@@ -1,10 +1,9 @@
 function employeeForm(){
 	let employeeId = document.getElementById("employeeId").value;
-	let regExp = "^\d{10}$";
 	validateEmployeeId(employeeId);
-	let firstName = document.getElementById("employeeFirstName").value;
+	let firstName = document.getElementById("firstName").value;
 	validateFirstName(firstName);
-	let lastName = document.getElementById("employeeLastName").value;
+	let lastName = document.getElementById("lastName").value;
 	validateLastName(lastName);
 	let email = document.getElementById("email").value;
 	validateEmail(email);
@@ -28,62 +27,101 @@ function employeeForm(){
 	console.log(employeeData);
 }      
  function validateEmployeeId(employeeId){
-	if (employeeId == "" || employeeId == null|| employeeId == regExp 	){
-	     document.getElementById("message").innerHTML = "please enter the employeeId";
+	if (employeeId == "" || employeeId == null){
+	     document.getElementById("employeeId").style.borderColor = "red";
 		 return "";
 	}
  }
- function validationEmployeeId(){
-	 if()
- }
+ 
+
 function validateFirstName(firstName){
 	 if(firstName == "" ||  firstName == null){
-		document.getElementById("message").innerHTML = "please enter the FirstName"; 
+		document.getElementById("firstName").style.borderColor = "red"; 
 	 }
  } 
 
 function validateLastName(lastName){
 	if(lastName == "" || lastName == null){
-	document.getElementById("message").innerHTML = "please enter the LastName";	
+	document.getElementById("lastName").style.borderColor = "red";	
 	}
 }
 function validateEmail(email){
 	if(email == "" || email == null ){
-		document.getElementById("message").innerHTML = "please enter the correct Email";
+		document.getElementById("email").style.borderColor = "red";
 	}
 }
 function validatePhoneNumber(phoneNumber){
 	if(phoneNumber == "" || phoneNumber == null){
-		document.getElementById("message").innerHTML = "please enter the correct Phone Number";
+		document.getElementById("phoneNumber").style.backgroundColor = "red";
 	}
 }
 function validateHireDate(hireDate){
 	if(hireDate == "" || hireDate == null){
-			document.getElementById("message").innerHTML = "please enter the Hire Date";
+			document.getElementById("hireDate").style.backgroundColor = "red";
 	}
 }
 function validateSalary(salary){
 	if(salary == "" || salary == null){
-		document.getElementById("message").innerHTML = "please enter the Salary";
+		document.getElementById("salary").style.backgroundColor = "red";
 	}
 }
 function validateJob(job){
 	if (job == "" || job == null){
-		document.getElementById("message").innerHTML = "please enter the job";
+		document.getElementById("job").style.backgroundColor = "yellow";
 	}
 }
 function validateCommisionPct(commisionPct){
 	if(commisionPct == "" || commisionPct == null){
-		document.getElementById("message").innerHTML = "please enter the CommisionPct";		
+		document.getElementById("commisionPct").style.backgroundColor = "yellow";		
 	}
 }
 function validateManager(manager){
 	if(manager == "" || manager == null){
-		document.getElementById("message").innerHTML = "please enter the manager";
+		document.getElementById("manager").style.backgroundColor = "yellow";
 	}
 }
 function validateDepartment(department){
 	if(department == "" || department == null){
-		document.getElementById("message").innerHTML = "please enter the Department";
+		document.getElementById("department").style.backgroundColor = "yellow";
 	}
+}
+function emailIdKeyPress(){
+	isSpace(event.keyCode);
+	isSpecialCharacter(event.key);
+	isAlphabet(event.key);
+}
+
+function isSpace(keyCode){
+		if (keyCode == 32) {
+		errorMessage("Space is not allowed here")
+		return false;
+	}
+}
+function isSpecialCharacter(key){
+	var scregexp = new RegExp(".!@#$%^&*()_+-=");
+	var patt = new RegExp(key);
+	 if (patt.test(scregexp)) {
+		let em = "special character not Required";
+		errorMessage(em);
+}
+}
+function isAlphabet(key){
+	
+	var cregexp = new RegExp("^[a-zA-Z]+$");
+	var patt = new RegExp(key);
+	 if (patt.test(cregexp)) {
+		let em = "character not allowed here";
+		errorMessage(em);
+}
+	
+}
+
+function successMessage(successMsg) {
+	document.getElementById("message").setAttribute("class","success-msg");
+	document.getElementById("message").innerHTML= successMsg;
+	//window.open("E:/App/html/home.html");
+}
+function errorMessage(errorMessage) {
+	document.getElementById("message").setAttribute("class", "error-msg");
+	document.getElementById("message").innerHTML = errorMessage;
 }
